37e7e40cab5614089c2076e58e23016f1dda540b
Yoshiyuki Hirano <yhirano@me.com>
2018-06-23 15:17:12 +0900

    Refactor routing

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -3,5 +3,5 @@
 #
 # Example:
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
-root to: 'home#index'
 resources :articles, only: [:index, :new, :create]
+root to: 'articles#index'
